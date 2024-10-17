9b478306fa619dc7b4ea10f982c1d0d931a46cc3
Viswanathkolachina <vkolachina@mgmresorts.com>
2024-10-17 14:27:32 -0500

    add files

--- scripts/process_mannequins.py
+++ scripts/process_mannequins.py
@@ -76,7 +76,7 @@ def validate_csv(csv_file):
     with open(csv_file, 'r') as file:
         csv_reader = csv.reader(file)
         header = next(csv_reader, None)
-        if header != ['mannequin_username', 'mannequin_id', 'role', 'target']:
+        if header != ['mannequin-user', 'mannequin-id', 'target', 'role']:
             raise ValueError("CSV file does not have the correct header format")
 
 def process_mannequins(csv_file):
